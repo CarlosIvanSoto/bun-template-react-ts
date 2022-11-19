f4f4fd051284eb489c3c29fffe60542ee586e0b1
Carlos Ivan Soto <gameue@hotmail.com>
2022-11-19 11:34:00 -0600

    template react typescrit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App" role="main">
+      <article className="App-article">
+        <h1>Hola mundo</h1>
+      </article>
+    </div>
+  );
+}
+
+export default App;
